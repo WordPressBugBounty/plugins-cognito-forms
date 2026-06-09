9975f702b4c9912327d77ef7e48647bf53feeb2b
Sean Murphy <sean@iamseanmurphy.com>
2026-06-09 20:52:06 +0000

    2.0.11

--- cognito-forms/dist/index.asset.php
+++ cognito-forms/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '5dc3f98faa5b73467a11');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '3da35db05d66022f4e2c');
